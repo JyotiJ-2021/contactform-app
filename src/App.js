8701ae593acefe611935770423bb35ca8e73e293
Jyoti <joytikumari12594@gmail.com>
2022-12-29 12:55:59 +0530

    changes on app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import AddContact from "./components/AddContact";
 import DisplayContact from "./components/DisplayContact";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import UpdateContact from "./components/UpdateContact";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter basename="contactform-app">
         <Routes>
           <Route exact path="/" element={<DisplayContact />} />
           <Route path="/add-contact" element={<AddContact />} />
           <Route path="/edit-contact/:id" element={<UpdateContact />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
